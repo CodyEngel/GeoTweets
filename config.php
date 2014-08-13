d95788c212db6a0465842a4d5cf72e293d08e8d9
Cody Engel <cengel815@gmail.com>
2014-08-12 20:03:17 -0500

    Dummy Config File
    This file is required for the project to work. You'll want to replace
    the default values with your own api keys.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$twitterOAuthToken			= 'twitter-oauth-token-here';
+$twitterOAuthSecret			= 'twitter-oauth-secrete-here';
+$twitterConsumerKey 		= 'twitter-consumer-key-here';
+$twitterConsumerKeySecret	= 'twitter-consumer-key-secrete-here';
+
+$googleMapsAPIKey			= 'google-maps-javascript-api-key-here';
+?>
